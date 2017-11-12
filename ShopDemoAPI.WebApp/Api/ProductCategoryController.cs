f10c6b2efc687d5dcdbf39e928400dc035413d6c
DESKTOP-9JFGFEM\nhatm <nhatminhit2705@gmail.com>
2017-11-12 15:19:08 +0700

    Paging for list

--- ShopDemoAPI.WebApp/Api/ProductCategoryController.cs
+++ ShopDemoAPI.WebApp/Api/ProductCategoryController.cs
@@ -15,20 +15,33 @@ namespace ShopDemoAPI.WebApp.Api
     [RoutePrefix("api/productcategory")]
     public class ProductCategoryController : ApiControllerBase
     {
-        IProductCategoryService _productCategoryService;
+        private IProductCategoryService _productCategoryService;
+
         public ProductCategoryController(IErrorService errorService, IProductCategoryService productCategoryService) : base(errorService)
         {
             this._productCategoryService = productCategoryService;
         }
 
         [Route("getall")]
-        public HttpResponseMessage Get(HttpRequestMessage request)
+        public HttpResponseMessage GetAll(HttpRequestMessage request, int page, int pageSize = 20)
         {
             return CreateHttpResponse(request, () =>
             {
+                int totalRow = 0;
                 var model = _productCategoryService.GetAll();
-                var responseData = Mapper.Map<IEnumerable<PRODUCTCATEGORY>, IEnumerable<PRODUCTCATEGORYViewModel>>(model);
-                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+                totalRow = model.Count();
+                var query = model.OrderByDescending(x => x.CREATEDDAY).Skip(page * pageSize).Take(pageSize);
+                var responseData = Mapper.Map<IEnumerable<PRODUCTCATEGORY>, IEnumerable<PRODUCTCATEGORYViewModel>>(query);
+
+                var paginationSet = new PaginationSet<PRODUCTCATEGORYViewModel>()
+                {
+                    Items = responseData,
+                    Page = page,
+                    TotalCount = totalRow,
+                    TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize)
+                };
+
+                var response = request.CreateResponse(HttpStatusCode.OK, paginationSet);
                 return response;
             });
         }
---
+++ ShopDemoAPI.WebApp/Infrastructure/Core/PaginationSet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ShopDemoAPI.WebApp.Infrastructure.Core
+{
+    public class PaginationSet<T>
+    {
+        public int Page { get; set; }
+
+        public int count
+        {
+            get
+            {
+                return (Items != null) ? Items.Count() : 0;
+            }
+        }
+
+        public int TotalPages { get; set; }
+
+        public int TotalCount { get; set; }
+
+        public IEnumerable<T> Items { get; set; }
+    }
+}
