bf487eba5fd789975a0202433f9cf7829a4abc5b
DESKTOP-9JFGFEM\nhatm <nhatminhit2705@gmail.com>
2017-11-12 16:50:09 +0700

    Add notification Service

---
+++ ShopDemoAPI.WebApp/app/shared/services/notificationService.js
@@ -0,0 +1,44 @@
+﻿(function (app) {
+    app.service('notificationService', notificationService);
+
+    function notificationService() {
+        toastr.options = {
+            "debug": false,
+            "positionClass": "toast-top-right",
+            "onclick": null,
+            "faceIn": 300,
+            "faceOut": 1000,
+            "timeOut": 3000,
+            "extendedTimeOut": 1000
+        };
+
+        function displaySuccess(message) {
+            toastr.success(message);
+        }
+
+        function displayError(message) {
+            if (Array.isArray(Error)) {
+                Error.each(function (err) {
+                    toastr.error(message);
+                });
+            } else {
+                toastr.error(message);
+            }
+        }
+
+        function displayWarning(message) {
+            toastr.warning(message);
+        }
+
+        function displayInfo(message) {
+            toastr.info(message);
+        }
+
+        return {
+            displaySuccess: displaySuccess,
+            displayError: displayError,
+            displayWarning: displayWarning,
+            displayInfo: displayInfo
+        }
+    }
+})(angular.module('shopdemoapi.common'));
